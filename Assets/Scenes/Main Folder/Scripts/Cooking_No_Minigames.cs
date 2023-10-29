4730b8217b661620e19b5662e79b61c12dd8dba9
Kirin Hardinger <khardinger@nevada.unr.edu>
2023-10-29 10:44:51 -0700

    Main Scene No Minigames
    Duplicate scene created to test gameplay without any cooking minigames. Duplicate scripts created for player interaction and cooking.

---
+++ Assets/Scenes/Main Folder/Scripts/Cooking_No_Minigames.cs
@@ -0,0 +1,68 @@
+// Kirin Hardinger
+// October 2023
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooking_No_Minigames : MonoBehaviour {
+    [Header("-----SPRITES-----")]
+    private SpriteRenderer sr;
+    public Sprite std;
+    public Sprite fire;
+
+    [Header("-----GAME OBJECTS-----")]
+    public GameObject dish;
+    [SerializeField] Timer timer_script;
+
+    [Header("-----STATE-----")]
+    bool cooking = false;
+    bool foodReady = false;
+    int cookTime = 5; // time in seconds, can be updated by upgrade system
+
+    void Start() {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+
+        //dish = GameObject.Find("dish");
+        //match = GameObject.Find("Match");
+
+        //timer_script = FindObjectOfType<Timer>();
+    }
+
+    public void SetCookTime(int newTime) {
+        cookTime = newTime;
+    }
+
+    public void StartCooking() {
+        cooking = true;
+        sr.sprite = fire;
+        StartCoroutine(CookWaiter(cookTime, PickupSystem.inst.GetItem())); // when ordering system is combined, this will be a variable passed in to StartCooking() from the oddering system
+    }
+
+    public bool IsCooking() {
+        return cooking;
+    }
+
+    public bool IsFoodReady()
+    {
+        return foodReady;
+    }
+
+    public void ResetCooktop()
+    {
+        dish.GetComponent<Food>().ResetDish();
+        foodReady = false;
+    }
+
+    // https://stackoverflow.com/questions/30056471/how-to-make-the-script-wait-sleep-in-a-simple-way-in-unity
+    IEnumerator CookWaiter(int sec, Sprite ingredient) {
+        PickupSystem.inst.DropItem();
+        timer_script.SetMaxTime(sec);
+        yield return new WaitForSeconds(sec);
+        sr.sprite = std;
+        dish.GetComponent<Food>().SetDish(ingredient); // https://forum.unity.com/threads/calling-function-from-other-scripts-c.57072/
+        Debug.Log("You have cooked something!");
+        cooking = false;
+        foodReady = true;
+    }
+}
