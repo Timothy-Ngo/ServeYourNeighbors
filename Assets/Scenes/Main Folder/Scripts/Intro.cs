1f4696854f3e1c6c0f6ac2fdf7d456e81c2a2d70
Kirin Hardinger <khardinger@nevada.unr.edu>
2024-02-07 18:39:34 -0800

    Intro Dialogue Skip Functionality
    - Advance intro dialogue using enter key
    - Pressing enter key in the middle of typewriter effect skips it, displays whole message
    - "SKIP" button available at any time to immediately take user to the game

--- Assets/Scenes/Main Folder/Scripts/Intro.cs
+++ Assets/Scenes/Main Folder/Scripts/Intro.cs
@@ -1,6 +1,7 @@
 // Kirin Hardinger
 // January 2024
 // Adapted from https://blog.yarsalabs.com/creating-a-dialogue-system-in-unity/ and https://gamedevbeginner.com/dialogue-systems-in-unity/
+// Updated 7-FEB-2024 to add skipping functionality. Referenced https://stackoverflow.com/questions/67774045/unity-fast-forward-type-writer-effect-upon-keypress
 
 using System.Collections;
 using System.Collections.Generic;
@@ -13,6 +14,9 @@ public class Intro : MonoBehaviour
     public DialogueAsset dialogueAsset;
     [SerializeField] TextMeshProUGUI dialogueText;
     [SerializeField] GameObject SYN;
+    [SerializeField] GameObject enterPrompt;
+    public bool skip = false;
+    public bool typing = false;
 
     float charsPerSec = 25;
 
@@ -20,26 +24,46 @@ public class Intro : MonoBehaviour
     {
         StartCoroutine(MoveThroughDialogue(dialogueAsset));
         SYN.SetActive(false);
+        enterPrompt.SetActive(false);
+    }
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.Return) && typing)
+        {
+            skip = true;
+        }
     }
 
     private IEnumerator MoveThroughDialogue(DialogueAsset dialogueAsset) {
-        for (int i = 0; i < dialogueAsset.dialogue.Length; i++) {
+        for (int i = 0; i < dialogueAsset.dialogue.Length; i++) 
+        {
+            enterPrompt.SetActive(false);
             string txtBuffer = "";
 
-            foreach (char c in dialogueAsset.dialogue[i]) {
+            foreach (char c in dialogueAsset.dialogue[i]) 
+            {
+                typing = true;
                 txtBuffer += c;
                 dialogueText.text = txtBuffer;
-                yield return new WaitForSeconds(1 / charsPerSec);
+                if(!skip) 
+                {
+                    yield return new WaitForSeconds(1 / charsPerSec);
+                }
             }
+            enterPrompt.SetActive(true);
+            skip = false;
+            typing = false;
 
-            //The following line of code makes it so that the for loop is paused until the user clicks the left mouse button.
-            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+            //The following line of code makes it so that the for loop is paused until the user presses the Enter key
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
             //The following line of codes make the coroutine wait for a frame so as the next WaitUntil is not skipped
             yield return null;
         }
         dialogueText.text = "";
         SYN.SetActive(true);
-        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        enterPrompt.SetActive(false);
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
         SceneManager.LoadScene("Main Scene");
     }
 }
