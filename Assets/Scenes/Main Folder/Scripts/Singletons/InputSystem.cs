0cbfed3a6b75721ed73b2f3c15caa1c750ba1999
Truong-Helen <helen.truong@nevada.unr.edu>
2024-03-21 23:26:37 -0700

    Adjusted Input System to restrict keybinds to unique keybinds + not allow keys used for movement and menu navigation

--- Assets/Scenes/Main Folder/Scripts/Singletons/InputSystem.cs
+++ Assets/Scenes/Main Folder/Scripts/Singletons/InputSystem.cs
@@ -2,6 +2,7 @@
 
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
 
 // currently, keys can be rebound to the same key -- may change later so that each key has to be unique
 
@@ -23,6 +24,8 @@ public class InputSystem : MonoBehaviour, ISettingsDataPersistence
     public TextMeshProUGUI pauseKeyLabel;
     public TextMeshProUGUI foodieSightKeyLabel;
 
+    public TextMeshProUGUI errorMsg;
+
     [Header("-----KEYCODES-----")]
     public KeyCode interactKey;
     public KeyCode cookKey;
@@ -31,6 +34,8 @@ public class InputSystem : MonoBehaviour, ISettingsDataPersistence
     public KeyCode finishPlacementKey;
     public KeyCode pauseKey;
     public KeyCode foodieSightKey;
+
+    List<KeyCode> reservedKeyCodes;
 
     [Header("-----FLAGS-----")]
     bool rebinding = false;
@@ -41,6 +46,8 @@ public class InputSystem : MonoBehaviour, ISettingsDataPersistence
     bool rebindingFinishPlacementKey = false;
     bool rebindingPauseKey = false;
     bool rebindingFoodieSightKey = false;
+
+
 
     public void LoadData(SettingsData data)
     {
@@ -66,6 +73,28 @@ public class InputSystem : MonoBehaviour, ISettingsDataPersistence
         data.foodieSightKey = foodieSightKey;
     }
 
+    void Start()
+    {
+        reservedKeyCodes = new List<KeyCode>();
+
+        reservedKeyCodes.Add(KeyCode.W);
+        reservedKeyCodes.Add(KeyCode.A);
+        reservedKeyCodes.Add(KeyCode.S);
+        reservedKeyCodes.Add(KeyCode.D);
+
+        reservedKeyCodes.Add(KeyCode.UpArrow);
+        reservedKeyCodes.Add(KeyCode.DownArrow);
+        reservedKeyCodes.Add(KeyCode.LeftArrow);
+        reservedKeyCodes.Add(KeyCode.RightArrow);
+
+        reservedKeyCodes.Add(KeyCode.Escape);
+
+        reservedKeyCodes.Add(KeyCode.Return);
+
+        // hide error message
+        errorMsg.gameObject.SetActive(false);
+    }
+
     public void SetLabels()
     {
         interactKeyLabel.text = interactKey.ToString();
@@ -84,55 +113,111 @@ public class InputSystem : MonoBehaviour, ISettingsDataPersistence
         if (e.isKey && e.keyCode != KeyCode.Return)//
         {
             //Debug.Log("Detected key code: " + e.keyCode);
-            
+
             if (rebinding)
             {
                 if (rebindingInteractKey)
                 {
-                    interactKeyLabel.text = e.keyCode.ToString();
-                    interactKey = e.keyCode;
+                    // if inputted key is available, set key to that
+                    if (KeyCodeAvailable(interactKey, e.keyCode))
+                    {
+                        interactKey = e.keyCode;
+                    }
+
+                    // update text
+                    interactKeyLabel.text = interactKey.ToString();
+
+                    // reset flags
                     rebindingInteractKey = false;
                     rebinding = false;
                 }
                 else if (rebindingCookKey)
                 {
-                    cookKeyLabel.text = e.keyCode.ToString();
-                    cookKey = e.keyCode;
+                    // if inputted key is available, set key to that
+                    if (KeyCodeAvailable(cookKey, e.keyCode))
+                    {
+                        cookKey = e.keyCode;
+                    }
+
+                    // update text
+                    cookKeyLabel.text = cookKey.ToString();
+                    
+                    // reset flags
                     rebindingCookKey = false;
                     rebinding = false;
                 }
                 else if (rebindingKidnapKey)
                 {
-                    kidnapKeyLabel.text = e.keyCode.ToString();
-                    kidnapKey = e.keyCode;
+                    // if inputted key is available, set key to that
+                    if (KeyCodeAvailable(kidnapKey, e.keyCode))
+                    {
+                        kidnapKey = e.keyCode;
+                    }
+
+                    // update text
+                    kidnapKeyLabel.text = kidnapKey.ToString();
+
+                    // reset flags
                     rebindingKidnapKey = false;
                     rebinding = false;
                 }
                 else if (rebindingServeKey)
                 {
-                    serveKeyLabel.text = e.keyCode.ToString();
-                    serveKey = e.keyCode;
+                    // if inputted key is available, set key to that
+                    if (KeyCodeAvailable(serveKey, e.keyCode))
+                    {
+                        serveKey = e.keyCode;
+                    }
+
+                    // update text
+                    serveKeyLabel.text = serveKey.ToString();
+                    
+                    // reset flags
                     rebindingServeKey = false;
                     rebinding = false;
                 }
                 else if (rebindingFinishPlacementKey)
                 {
-                    finishPlacementKeyLabel.text = e.keyCode.ToString();
-                    finishPlacementKey = e.keyCode;
+                    // if inputted key is available, set key to that
+                    if (KeyCodeAvailable(finishPlacementKey, e.keyCode))
+                    {
+                        finishPlacementKey = e.keyCode;
+                    }
+
+                    // update text
+                    finishPlacementKeyLabel.text = finishPlacementKey.ToString();
+                    
+                    // reset flags
                     rebindingFinishPlacementKey = false;
                     rebinding = false;
                 }
                 else if (rebindingPauseKey)
                 {
-                    pauseKeyLabel.text = e.keyCode.ToString();
-                    pauseKey = e.keyCode;
+                    // if inputted key is available, set key to that
+                    if (KeyCodeAvailable(pauseKey, e.keyCode))
+                    {
+                        pauseKey = e.keyCode;
+                    }
+
+                    // update text
+                    pauseKeyLabel.text = pauseKey.ToString();
+                    
+                    // reset flags
                     rebindingPauseKey = false;
                     rebinding = false;
                 }
                 else if (rebindingFoodieSightKey)
                 {
-                    foodieSightKeyLabel.text = e.keyCode.ToString();
-                    foodieSightKey = e.keyCode;
+                    // if inputted key is available, set key to that
+                    if (KeyCodeAvailable(foodieSightKey, e.keyCode))
+                    {
+                        foodieSightKey = e.keyCode;
+                    }
+
+                    // update text
+                    foodieSightKeyLabel.text = foodieSightKey.ToString();
+                    
+                    // reset flags
                     rebindingFoodieSightKey = false;
                     rebinding = false;
                 }
@@ -140,66 +225,142 @@ public class InputSystem : MonoBehaviour, ISettingsDataPersistence
         }
     }
 
+    // checks if a given keyCode is the same as any in-use keycodes
+    bool KeyCodeInUse(KeyCode key)
+    {
+        if (key == interactKey || key == cookKey || key == kidnapKey || key == serveKey || key == finishPlacementKey || key == pauseKey || key == foodieSightKey)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // checks if a given keyCode is available to bind to
+    bool KeyCodeAvailable(KeyCode prevKey, KeyCode inputtedKey)
+    {
+        if (prevKey != inputtedKey)
+        {
+            // check if keyCode is available to be used
+            if (reservedKeyCodes.Contains(inputtedKey)) // if inputted key is a reserved KeyCode -- don't allow rebinding
+            {
+                // key binding remains unchanged
+
+                // show error message
+                errorMsg.gameObject.SetActive(true);
+                errorMsg.text = "Cannot set key: key is reserved for movement or menu navigation";
+
+                return false;
+            }
+            else if (!KeyCodeInUse(inputtedKey)) // if inputted key is used by another binding (aka is not available) -- don't allow rebinding
+            {
+                // key binding remains unchanged
+
+                // show error message
+                errorMsg.gameObject.SetActive(true);
+                errorMsg.text = "Cannot set key: key is already in use";
+
+                return false;
+            }
+            else // key is available
+            {
+                return true;
+            }
+        }
+
+        // key and inputted key are the same -- key is technically available
+        return true;
+    }
+
+    // functions assigned to OnClick() in inspector
     public void RebindInteractKey()
     {
-        rebinding = true;
+        // hide error msg
+        errorMsg.gameObject.SetActive(false);
 
+        // change key label to show active rebinding state
         interactKeyLabel.text = "[Press Key]";
 
+        // set flags
+        rebinding = true;
         rebindingInteractKey = true;
     }
 
     public void RebindCookKey()
     {
-        rebinding = true;
+        // hide error msg
+        errorMsg.gameObject.SetActive(false);
 
+        // change key label to show active rebinding state
         cookKeyLabel.text = "[Press Key]";
 
+        // set flags
+        rebinding = true;
         rebindingCookKey = true;
     }
 
     public void RebindKidnapKey()
     {
-        rebinding = true;
+        // hide error msg
+        errorMsg.gameObject.SetActive(false);
 
+        // change key label to show active rebinding state
         kidnapKeyLabel.text = "[Press Key]";
 
+        // set flags
+        rebinding = true;
         rebindingKidnapKey = true;
     }
 
     public void RebindServeKey()
     {
-        rebinding = true;
+        // hide error msg
+        errorMsg.gameObject.SetActive(false);
 
+        // change key label to show active rebinding state
         serveKeyLabel.text = "[Press Key]";
 
+        // set flags
+        rebinding = true;
         rebindingServeKey = true;
     }
 
     public void RebindFinishPlacementKey()
     {
-        rebinding = true;
+        // hide error msg
+        errorMsg.gameObject.SetActive(false);
 
+        // change key label to show active rebinding state
         finishPlacementKeyLabel.text = "[Press Key]";
 
+        // set flags
+        rebinding = true;
         rebindingFinishPlacementKey = true;
     }
 
     public void RebindPauseKey()
     {
-        rebinding = true;
+        // hide error msg
+        errorMsg.gameObject.SetActive(false);
 
+        // change key label to show active rebinding state
         pauseKeyLabel.text = "[Press Key]";
 
+        // set flags
+        rebinding = true;
         rebindingPauseKey = true;
     }
 
     public void RebindFoodieSightKey()
     {
-        rebinding = true;
+        // hide error msg
+        errorMsg.gameObject.SetActive(false);
 
+        // change key label to show active rebinding state
         foodieSightKeyLabel.text = "[Press Key]";
 
+        // set flags
+        rebinding = true;
         rebindingFoodieSightKey = true;
     }
 
